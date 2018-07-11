19117a515587ecb7614a3f7d8e367dfb753fb912
Efi <Efi@Efi_Mario>
2018-07-11 08:56:09 -0600

    Unificacion del proyecto

--- Control_Salones/src/control_salones/datos/Conector.java
+++ Control_Salones/src/control_salones/datos/Conector.java
@@ -1,14 +1,11 @@
 package control_salones.datos;
 
-import control_salones.modelo.Salon;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Conector {
 
